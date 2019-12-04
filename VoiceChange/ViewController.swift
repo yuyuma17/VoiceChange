4154c8b5f1cbedc4af0507bb7a59f9e201dcfc6d
Lacie <dark830917@gmail.com>
2019-12-04 23:49:06 +0800

    Initial Commit

---
+++ VoiceChange/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VoiceChange
+//
+//  Created by 黃士軒 on 2019/12/4.
+//  Copyright © 2019 Lacie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
