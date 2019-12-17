17c81aba3302f1b336bc2dadba97c23e04997c64
Lacie <dark830917@gmail.com>
2019-12-17 22:15:09 +0800

    set up recordUI

--- VoiceChange/ViewController.swift
+++ VoiceChange/ViewController.swift
@@ -10,11 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        stopRecordingButton.isEnabled = false
     }
 
-
+    @IBAction func recordAudio(_ sender: UIButton) {
+        
+        recordingLabel.text = "Recording in Progress"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
+    
+    @IBAction func stopRecording(_ sender: UIButton) {
+        
+        recordButton.isEnabled = true
+        stopRecordingButton.isEnabled = false
+        recordingLabel.text = "Tap to Record"
+    }
+    
 }
 
