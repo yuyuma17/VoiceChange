a732f9d117ae848f5f7e6d593b8616350d3751e9
Lacie <dark830917@gmail.com>
2019-12-27 23:52:49 +0800

    準備傳送音檔 URL 到第二個 VC

---
+++ VoiceChange/PlaySoundsViewController.swift
@@ -0,0 +1,25 @@
+//
+//  PlaySoundsViewController.swift
+//  VoiceChange
+//
+//  Created by 黃士軒 on 2019/12/27.
+//  Copyright © 2019 Lacie. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    var recordedAudioURL: URL!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+    
+    
+    
+    
+}
--- VoiceChange/RecordSoundsViewController.swift
+++ VoiceChange/RecordSoundsViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import AVFoundation
 
-class RecordSoundsViewController: UIViewController {
+class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
     var audioRecorder: AVAudioRecorder!
     
@@ -39,6 +39,7 @@ class RecordSoundsViewController: UIViewController {
         try! session.setCategory(AVAudioSession.Category.playAndRecord, mode: AVAudioSession.Mode.default, options: AVAudioSession.CategoryOptions.defaultToSpeaker)
 
         try! audioRecorder = AVAudioRecorder(url: filePath!, settings: [:])
+        audioRecorder.delegate = self
         audioRecorder.isMeteringEnabled = true
         audioRecorder.prepareToRecord()
         audioRecorder.record()
@@ -53,6 +54,15 @@ class RecordSoundsViewController: UIViewController {
         audioRecorder.stop()
         let audioSession = AVAudioSession.sharedInstance()
         try! audioSession.setActive(false)
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
+        
+        if flag {
+            performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
+        } else {
+            print("Recording was not successful")
+        }
     }
     
 }
