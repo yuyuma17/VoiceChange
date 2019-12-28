734e87af35106cf71b38d3b23494b4383484b9de
Lacie <dark830917@gmail.com>
2019-12-29 00:42:28 +0800

    完成錄音及各項變音

--- VoiceChange/RecordSoundsViewController.swift
+++ VoiceChange/RecordSoundsViewController.swift
@@ -65,5 +65,14 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
         }
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "stopRecording" {
+            let playSoundsVC = segue.destination as! PlaySoundsViewController
+            let recordedAudioURL = sender as! URL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+        }
+    }
+    
 }
 
